4dd09e584ff133a3b650ebcbc0060539087da453
Nikolay Novikov <AnourValar@gmail.com>
2025-09-24 19:19:35 +0300

    patch

--- src/FilesystemCheck.php
+++ src/FilesystemCheck.php
@@ -13,12 +13,28 @@ class FilesystemCheck extends Check
     protected ?array $disks = [];
 
     /**
+     * @var bool
+     */
+    protected bool $withDirectory = true;
+
+    /**
      * @param array $disks
      * @return self
      */
     public function disks(array $disks): self
     {
         $this->disks = $disks;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $withDirectory
+     * @return self
+     */
+    public function withDirectory(bool $withDirectory): self
+    {
+        $this->withDirectory = $withDirectory;
 
         return $this;
     }
@@ -49,12 +65,22 @@ class FilesystemCheck extends Check
                 return $result->failed("Disk \"$disk\" is not reachable.");
             }
 
-            do {
-                $path = 'temp_' . sha1(\Str::random(50, 200));
-            } while (\Storage::disk($disk)->exists($path));
+            if ($this->withDirectory) {
+                do {
+                    $path = 'temp_' . sha1(\Str::random(50, 200)) . '/';
+                } while (\Storage::disk($disk)->exists($path));
+            } else {
+                $path = '';
+            }
 
-            if ($error = $this->checkFlow($disk, $path, 'file', $hasUrl)) {
-                \Storage::disk($disk)->deleteDirectory($path);
+            do {
+                $file = 'temp_' . sha1(\Str::random(50, 200));
+            } while (\Storage::disk($disk)->exists("{$path}{$file}"));
+
+            if ($error = $this->checkFlow($disk, $path, $file, $hasUrl)) {
+                if ($path) {
+                    \Storage::disk($disk)->deleteDirectory($path);
+                }
 
                 return $result->failed($error);
             }
@@ -72,44 +98,72 @@ class FilesystemCheck extends Check
      */
     private function checkFlow(string $disk, string $path, string $file, bool $hasUrl): ?string
     {
+        // create directory?
         try {
-            \Storage::disk($disk)->makeDirectory($path);
+            if ($path) {
+                \Storage::disk($disk)->makeDirectory($path);
+            }
         } catch (\Exception $e) {
             return "Disk \"$disk\": cannot make a directory.";
         }
 
+        // put file
         try {
-            \Storage::disk($disk)->put("$path/$file", 'foo');
+            \Storage::disk($disk)->put("{$path}{$file}", 'foo');
         } catch (\Exception $e) {
             return "Disk \"$disk\": cannot put a file.";
         }
 
+        // get file in list
         try {
-            if (\Storage::disk($disk)->get("$path/$file") != 'foo') {
+            if (! in_array("{$path}{$file}", \Storage::disk($disk)->files($path))) {
+                return "Disk \"$disk\": cannot get a file in the list.";
+            }
+        } catch (\Exception $e) {
+            return "Disk \"$disk\": cannot get a file in the list.";
+        }
+
+        // get file
+        try {
+            if (\Storage::disk($disk)->get("{$path}{$file}") != 'foo') {
                 return "Disk \"$disk\": cannot get a file.";
             }
         } catch (\Exception $e) {
             return "Disk \"$disk\": cannot get a file.";
         }
 
-        if ($hasUrl) {
-            try {
-                if (file_get_contents(\Storage::disk($disk)->url("$path/$file")) != 'foo') {
-                    return "Disk \"$disk\": cannot fetch (via url) a file.";
-                }
-            } catch (\Exception $e) {
-                return "Disk \"$disk\": cannot fetch (via url) a file.";
-            }
+        // access file from direct link?
+        $context = stream_context_create(['http' => ['method' => 'GET', 'ignore_errors' => true]]);
+        $fetch = file_get_contents(\Storage::disk($disk)->url("{$path}{$file}"), false, $context);
+        $headers = json_encode($http_response_header);
+        if ($hasUrl && ($fetch != 'foo' || ! stripos($headers, ' 200 '))) {
+            return "Disk \"$disk\": cannot fetch (via url) a file.";
+        }
+        if (! $hasUrl && (! stripos($fetch, 'AccessDenied') || ! stripos($headers, ' 403 '))) {
+            return "Disk \"$disk\": can fetch (via url) a private file.";
         }
 
+        // access file from temporary link
         try {
-            \Storage::disk($disk)->delete("$path/$file");
+            if (file_get_contents(\Storage::disk($disk)->temporaryUrl("{$path}{$file}", now()->addSeconds(10))) != 'foo') {
+                return "Disk \"$disk\": cannot fetch (via temporary url) a file.";
+            }
+        } catch (\Exception $e) {
+            return "Disk \"$disk\": cannot fetch (via temporary url) a file.";
+        }
+
+        // delete file
+        try {
+            \Storage::disk($disk)->delete("{$path}{$file}");
         } catch (\Exception $e) {
             return "Disk \"$disk\": cannot delete a file.";
         }
 
+        // delete directory?
         try {
-            \Storage::disk($disk)->deleteDirectory($path);
+            if ($path) {
+                \Storage::disk($disk)->deleteDirectory($path);
+            }
         } catch (\Exception $e) {
             return "Disk \"$disk\": cannot delete a directory.";
         }
